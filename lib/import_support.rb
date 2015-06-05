bbd2202f2aaef7f1fb1fa9e3f9ca25426e58afd8
ttriggs <tyler.triggs@gmail.com>
2015-06-04 22:54:17 -0400

    update #parse_record to use #hash_record_from_array

--- lib/import_support.rb
+++ lib/import_support.rb
@@ -15,5 +15,17 @@ module ImportSupport
 
   def parse_record(record)
     array = record.split(delimiter(record)).map(&:strip)
+    hash_record_from_array(array)
+  end
+
+  def hash_record_from_array(array)
+    {
+      last_name: array[0],
+      first_name: array[1],
+      gender: array[2],
+      favorite_color: array[3],
+      dob: array[4],
+      display_dob: display_dob(array[4])
+    }
   end
 end
