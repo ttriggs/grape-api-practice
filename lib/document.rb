84a36707d07c2510664612e32a440182fc54d460
ttriggs <tyler.triggs@gmail.com>
2015-06-05 01:16:38 -0400

    add #import_records_to_redis

--- lib/document.rb
+++ lib/document.rb
@@ -1,7 +1,9 @@
 require_relative 'import_support'
+require_relative 'redis_client'
 
 class Document
   include ImportSupport
+  include RedisClient
   attr_reader :file
 
   def initialize(file)
@@ -11,6 +13,12 @@ class Document
   def records
     File.readlines(@file).each_with_object([]) do |line, records|
       records << parse_record(line)
+    end
+  end
+
+  def import_records_to_redis
+    records.each do |record|
+      import_record(record)
     end
   end
 end
