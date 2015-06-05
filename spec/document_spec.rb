6ceb381be3b26cf6a4f9c14ee8bb0f6029a30f8a
ttriggs <tyler.triggs@gmail.com>
2015-06-04 22:52:40 -0400

    add #records

--- spec/document_spec.rb
+++ spec/document_spec.rb
@@ -1,31 +1,14 @@
 require 'spec_helper'
 
 describe "Document class" do
-  let (:comma_file) { './spec/example_seeds/comma_delimiter.txt' }
-  let (:pipe_file) { './spec/example_seeds/pipe_delimiter.txt' }
-  let (:space_file) { './spec/example_seeds/space_delimiter.txt' }
+  let (:pipe_file) { Document.new('./spec/example_seeds/pipe_delimiter.txt') }
 
-  context ".initialize" do
-    it "assigns #delimiter to ',' when given comma delimited file" do
-      file = Document.new(comma_file)
-      expect(file.delimiter).to eq(",")
-    end
-
-    it "assigns #delimiter to '|' when given pipe delimited file" do
-      file = Document.new(pipe_file)
-      expect(file.delimiter).to eq("|")
-    end
-
-    it "assigns #delimiter to ' ' when given space delimited file" do
-      file = Document.new(space_file)
-      expect(file.delimiter).to eq(" ")
-    end
-  end
-
-  context "#first_line" do
-    it "returns first line of file" do
-      response = Document.new(pipe_file).first_line
-      expect(response).to eq("Zachary | Albert | male | red | 19900521")
+  context "#records" do
+    it "returns array of hashes for records in @file" do
+      response = pipe_file.records
+      number_records_expected = File.foreach(pipe_file.file).count
+      expect(response.length).to eq(number_records_expected)
+      expect(response.first.class).to be(Hash)
     end
   end
 end
