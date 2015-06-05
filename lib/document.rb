8cadab309e5f7c2df47bc55e5e189d1d131f9bf0
ttriggs <tyler.triggs@gmail.com>
2015-06-04 21:33:58 -0400

    include import support for get_delimiter

--- lib/document.rb
+++ lib/document.rb
@@ -1,22 +1,18 @@
+require_relative 'import_support'
+
 class Document
+  include ImportSupport
   attr_reader :delimiter
 
   def initialize(file)
     @file = file
-    @delimiter = Document.get_delimiter(first_line)
+  end
+
+  def delimiter
+    @delimiter ||= get_delimiter(first_line)
   end
 
   def first_line
     File.open(@file, &:readline).strip
-  end
-
-  def self.get_delimiter(record)
-    if record.include?("|")
-      "|"
-    elsif record.include?(",")
-      ","
-    else
-      " "
-    end
   end
 end
