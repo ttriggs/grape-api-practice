a2e8188dbf5f941f756cd972d899c2df4d6a3fdd
ttriggs <tyler.triggs@gmail.com>
2015-06-05 01:16:11 -0400

    add #import_records_to_redis test

--- spec/document_spec.rb
+++ spec/document_spec.rb
@@ -2,13 +2,25 @@ require 'spec_helper'
 
 describe "Document class" do
   let (:pipe_file) { Document.new('./spec/example_seeds/pipe_delimiter.txt') }
+  let (:redis) { Redis.new }
+  before(:each) { redis.flushdb }
 
   context "#records" do
     it "returns array of hashes for records in @file" do
       response = pipe_file.records
-      number_records_expected = File.foreach(pipe_file.file).count
-      expect(response.length).to eq(number_records_expected)
+      expected_records = File.foreach(pipe_file.file).count
+      expect(response.length).to eq(expected_records)
       expect(response.first.class).to be(Hash)
+    end
+  end
+
+  context "#import_records_to_redis" do
+    it "adds all new records to redis db" do
+      expected_records = File.foreach(pipe_file.file).count
+      pipe_file.import_records_to_redis
+      total_records = redis.smembers("records").count
+
+      expect(total_records).to eq(expected_records)
     end
   end
 end
