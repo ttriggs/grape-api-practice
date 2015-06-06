aeb4320eee9b244a5f3eddcbba875fe419206c9b
ttriggs <tyler.triggs@gmail.com>
2015-06-06 15:10:39 -0400

    require coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,15 @@
 require 'rubygems'
+require 'rack/test'
+require 'coveralls'
+Coveralls.wear!
 
 ENV['RACK_ENV'] ||= 'test'
-
 
 require File.expand_path('../../config/environment', __FILE__)
 
 Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].each do |f|
   require f
 end
-
-require 'rack/test'
 
 RSpec.configure do |config|
   config.mock_with :rspec
