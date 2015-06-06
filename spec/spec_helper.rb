b15a00dad682ac11277c8b2a769028e97dc802b1
ttriggs <tyler.triggs@gmail.com>
2015-06-05 20:38:45 -0400

    require support file output_catcher for gli testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,9 +4,15 @@ ENV['RACK_ENV'] ||= 'test'
 
 require File.expand_path('../../config/environment', __FILE__)
 
+Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].each do |f|
+  require f
+end
+
 RSpec.configure do |config|
   config.mock_with :rspec
   config.color = true
   config.expect_with :rspec
   config.raise_errors_for_deprecations!
+  config.order = :random
+  config.include OutputCatcher
 end
