de890beb7dcce3607607b567ed06a71b52beda41
ttriggs <tyler.triggs@gmail.com>
2015-06-07 15:53:47 -0400

    add #dob_mutate_records

--- lib/import_support.rb
+++ lib/import_support.rb
@@ -13,6 +13,12 @@ module ImportSupport
     Date.parse(dob).strftime("%-m/%-d/%Y")
   end
 
+  def dob_mutate_records(records)
+    records.each do |record|
+      record["dob"] = display_dob(record["dob"])
+    end
+  end
+
   def parse_record(record)
     array = record.split(delimiter(record)).map(&:strip)
     hash_record_from_array(array)
@@ -24,8 +30,7 @@ module ImportSupport
       first_name: array[1],
       gender: array[2],
       favorite_color: array[3],
-      dob: array[4],
-      display_dob: display_dob(array[4])
+      dob: array[4]
     }
   end
 end
